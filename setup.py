5261364242314baaf0199cc34fcfbd82190f2071
msd <87383399+msd@users.noreply.github.com>
2023-07-13 18:10:33 +0100

    Fix: Now can be installed

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 from pathlib import Path
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -8,18 +9,19 @@ from pathlib import Path
 def read(fname: str) -> str:
     return (Path(__file__).parent / fname).read_text(encoding="utf-8")
 
+
 setup(
-    name = "msd_common",
-    version = "0.0.1",
-    author = "msd",
-    author_email = "87383399+msd@users.noreply.github.com",
-    description = "common functions and classes used by msd",
+    name="msd_common",
+    version="0.0.1",
+    author="msd",
+    author_email="87383399+msd@users.noreply.github.com",
+    description="common functions and classes used by msd",
     # license = "BSD",
     # keywords = "example documentation tutorial",
-    install_requires = ["requests", "brotli", "bs4", "lxml"]
-    url = "https://github.com/msd/my_common",
+    install_requires=["requests", "brotli", "bs4", "lxml"],
+    url="https://github.com/msd/my_common",
     packages=["msd_common"],
-    long_description=read('readme')
+    long_description=read("readme")
     # classifiers=[
     #     "Development Status :: 3 - Alpha",
     #     "Topic :: Utilities",
