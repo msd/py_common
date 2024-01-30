5eb872aa87eb479561cc10769ed52aee69c14f14
msd <87383399+msd@users.noreply.github.com>
2024-01-30 19:45:57 +0000

    Fix: installation fails

--- setup.py
+++ setup.py
@@ -1,27 +1,17 @@
 from setuptools import setup
-from pathlib import Path
-
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname: str) -> str:
-    return (Path(__file__).parent / fname).read_text(encoding="utf-8")
-
 
 setup(
     name="msd_common",
     version="0.0.1",
     author="msd",
     author_email="87383399+msd@users.noreply.github.com",
-    description="stuff I use all the time",
+    description="Utility library",
     # license = "BSD",
     # keywords = "example documentation tutorial",
     install_requires=["requests", "brotli", "bs4", "lxml"],
-    url="https://github.com/msd/my_common",
+    url="https://github.com/msd/py_common",
     packages=["msd_common"],
-    long_description=read("readme")
+    long_description="Utility functions for http headers, downloading files"
     # classifiers=[
     #     "Development Status :: 3 - Alpha",
     #     "Topic :: Utilities",
